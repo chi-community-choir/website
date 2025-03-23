0c4a5d2a3056a5f6caed61ad18e77fcaaee48dd7
jknight-developer <jonathan@jknightdev.co.uk>
2025-03-23 20:31:03 +0000

    try using fetchGit to ensure correct versioning

--- shell.nix
+++ shell.nix
@@ -1,4 +1,10 @@
-{ pkgs ? import <nixpkgs> { } }:
+{ pkgs ? import (builtins.fetchGit {
+      name = "nixos-unstable-2025-03-25";
+      url = "https://github.com/NixOS/nixpkgs";
+      ref = "refs/heads/nixos-unstable";
+      rev = "bfa9810ff7104a17555ab68ebdeafb6705f129b1";
+    }) {}
+}:
 pkgs.mkShell {
     buildInputs = with pkgs; [ tmux git gleam erlang_27 rebar3 bun ];
 }
