9fb78550fe4d431df99329586f4f1b520a72bfa8
Jonathan Knight <jonathan@jknightdev.co.uk>
2025-09-03 16:36:18 +0100

    guard against failed build steps

--- run-dev.sh
+++ run-dev.sh
@@ -1,3 +1,4 @@
+set -euo pipefail
 cd ./client
 bun install
 echo "pub fn get_api_url() -> String { \"${API_URL}\" }" > ./src/client/env.gleam
--- run-ssr.sh
+++ run-ssr.sh
@@ -1,4 +1,7 @@
+set -euo pipefail
 cd ./client
+bun install
+echo "pub fn get_api_url() -> String { \"${API_URL}\" }" > ./src/client/env.gleam
 gleam run -m lustre/dev build --outdir=../server/priv/static --minify
 cd ../server
 gleam run
