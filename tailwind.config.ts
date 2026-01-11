6cb8c84867087a2924d4e470f5831ecf47ec077e
Jonathan Knight <jonathan@jknightdev.co.uk>
2026-01-11 22:55:21 +0000

    full example song

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,19 @@ const config: Config = {
         'choir-blue': '#2c5282',
         'choir-blue-light': '#2a4365',
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            'img': {
+              maxWidth: '65%',
+              marginLeft: 'auto',
+              marginRight: 'auto',
+              borderRadius: '0.5rem',
+              boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1)',
+            },
+          },
+        },
+      },
     },
   },
   plugins: [
