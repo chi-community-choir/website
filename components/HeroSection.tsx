a5dbc707fbda08e0ffe9f92b2bbcef14a6ae42ac
Jonathan Knight <jonathan@jknightdev.co.uk>
2026-01-11 20:23:16 +0000

    test vercel CDN for banner image

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -3,7 +3,7 @@ export default function HeroSection() {
     <section className="py-8">
       <div className="container mx-auto flex justify-center items-center px-4">
         <img
-          src="https://chicommunitychoir.lon1.cdn.digitaloceanspaces.com/2019-12-13-Choir-Header-1.png"
+          src="https://flhaurczyt40eicf.public.blob.vercel-storage.com/images/2019-12-13-Choir-Header-1.png"
           alt="Chichester Community Choir banner"
           className="rounded-lg max-w-[90%] h-auto shadow-md"
         />
