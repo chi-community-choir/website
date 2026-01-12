919685fc8570ee9243a529f2f5d8a75002ee5df0
Jonathan Knight <jonathan@jknightdev.co.uk>
2026-01-12 00:33:03 +0000

    search functionality

---
+++ app/repertoire/layout.tsx
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Repertoire | Chichester Community Choir',
+  description: 'Our current and past repertoire',
+}
+
+export default function RepertoireLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return children
+}
