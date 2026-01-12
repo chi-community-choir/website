811f2572bcf0938353d5768c8bfb9856d6cd1766
Jonathan Knight <jonathan@jknightdev.co.uk>
2026-01-12 00:25:16 +0000

    use next/image

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -1,8 +1,10 @@
+import Image from 'next/image'
+
 export default function HeroSection() {
   return (
     <section className="py-8">
       <div className="container mx-auto flex justify-center items-center px-4">
-        <img
+        <Image
           src="https://flhaurczyt40eicf.public.blob.vercel-storage.com/images/2019-12-13-Choir-Header-1.png"
           alt="Chichester Community Choir banner"
           className="rounded-lg max-w-[90%] h-auto shadow-md"
