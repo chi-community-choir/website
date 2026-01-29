a2a03b77351cdd38da6c56880d0c74f073adc437
Jonathan Knight <jonathan@jknightdev.co.uk>
2026-01-29 22:27:19 +0000

    hero/cta margin tweaks

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -2,8 +2,8 @@ import Image from 'next/image'
 
 export default function HeroSection() {
   return (
-    <section className="py-8">
-      <div className="max-w-7xl mx-auto px-4">
+    <section className="py-8 px-4">
+      <div className="max-w-7xl mx-auto">
         <div className="bg-gray-200 rounded-lg overflow-hidden">
           <Image
             src="https://flhaurczyt40eicf.public.blob.vercel-storage.com/images/2019-12-13-Choir-Header-1.png"
